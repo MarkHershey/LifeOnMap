50328adfe432a1b0589d9a94ec87bde70d7da216
Mark Huang <hhzeos@gmail.com>
2022-08-09 21:56:24 +0800

    Add tailwind and react-leaflet

--- map-app/src/App.tsx
+++ map-app/src/App.tsx
@@ -1,5 +1,52 @@
+import { LatLngExpression } from "leaflet";
+import {
+    MapContainer,
+    TileLayer,
+    // useMap,
+    // Circle,
+    // CircleMarker,
+    Polyline,
+    // Polygon,
+    // Rectangle,
+    // Popup,
+} from "react-leaflet";
+
+const center: LatLngExpression = [51.505, -0.09];
+
+const polyline: LatLngExpression[] = [
+    [51.505, -0.09],
+    [51.51, -0.1],
+    [51.51, -0.12],
+];
+
+const multiPolyline: LatLngExpression[][] = [
+    [
+        [51.5, -0.1],
+        [51.5, -0.12],
+        [51.52, -0.12],
+    ],
+    [
+        [51.5, -0.05],
+        [51.5, -0.06],
+        [51.52, -0.06],
+    ],
+];
+const limeOptions = { color: "lime" };
+
 function App() {
-    return <div className="">Hello</div>;
+    return (
+        <div className="m-3 border-5 border-blue-800 w-10 h-10 bg-slate-600">
+            {/* <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+
+                <Polyline pathOptions={limeOptions} positions={polyline} />
+                <Polyline pathOptions={limeOptions} positions={multiPolyline} />
+            </MapContainer> */}
+        </div>
+    );
 }
 
 export default App;
