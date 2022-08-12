f6a5804d92dff09269e6b6573c8af1f6ef6b82ca
Mark Huang <hhzeos@gmail.com>
2022-08-13 00:16:38 +0800

    Clean up

--- map-app/src/assets/avatars.ts
+++ map-app/src/assets/avatars.ts
@@ -49,6 +49,88 @@ import Img47 from "./profiles/47.jpg";
 import Img48 from "./profiles/48.jpg";
 import Img49 from "./profiles/49.jpg";
 import Img50 from "./profiles/50.jpg";
+import Img51 from "./profiles/51.jpg";
+import Img52 from "./profiles/52.jpg";
+import Img53 from "./profiles/53.jpg";
+import Img54 from "./profiles/54.jpg";
+import Img55 from "./profiles/55.jpg";
+import Img56 from "./profiles/56.jpg";
+import Img57 from "./profiles/57.jpg";
+import Img58 from "./profiles/58.jpg";
+import Img59 from "./profiles/59.jpg";
+import Img60 from "./profiles/60.jpg";
+import Img61 from "./profiles/61.jpg";
+import Img62 from "./profiles/62.jpg";
+import Img63 from "./profiles/63.jpg";
+import Img64 from "./profiles/64.jpg";
+import Img65 from "./profiles/65.jpg";
+import Img66 from "./profiles/66.jpg";
+import Img67 from "./profiles/67.jpg";
+import Img68 from "./profiles/68.jpg";
+import Img69 from "./profiles/69.jpg";
+import Img70 from "./profiles/70.jpg";
+import Img71 from "./profiles/71.jpg";
+import Img72 from "./profiles/72.jpg";
+import Img73 from "./profiles/73.jpg";
+import Img74 from "./profiles/74.jpg";
+import Img75 from "./profiles/75.jpg";
+import Img76 from "./profiles/76.jpg";
+import Img77 from "./profiles/77.jpg";
+import Img78 from "./profiles/78.jpg";
+import Img79 from "./profiles/79.jpg";
+import Img80 from "./profiles/80.jpg";
+import Img81 from "./profiles/81.jpg";
+import Img82 from "./profiles/82.jpg";
+import Img83 from "./profiles/83.jpg";
+import Img84 from "./profiles/84.jpg";
+import Img85 from "./profiles/85.jpg";
+import Img86 from "./profiles/86.jpg";
+import Img87 from "./profiles/87.jpg";
+import Img88 from "./profiles/88.jpg";
+import Img89 from "./profiles/89.jpg";
+import Img90 from "./profiles/90.jpg";
+import Img91 from "./profiles/91.jpg";
+import Img92 from "./profiles/92.jpg";
+import Img93 from "./profiles/93.jpg";
+import Img94 from "./profiles/94.jpg";
+import Img95 from "./profiles/95.jpg";
+import Img96 from "./profiles/96.jpg";
+import Img97 from "./profiles/97.jpg";
+import Img98 from "./profiles/98.jpg";
+import Img99 from "./profiles/99.jpg";
+import Img100 from "./profiles/100.jpg";
+import Img101 from "./profiles/101.jpg";
+import Img102 from "./profiles/102.jpg";
+import Img103 from "./profiles/103.jpg";
+import Img104 from "./profiles/104.jpg";
+import Img105 from "./profiles/105.jpg";
+import Img106 from "./profiles/106.jpg";
+import Img107 from "./profiles/107.jpg";
+import Img108 from "./profiles/108.jpg";
+import Img109 from "./profiles/109.jpg";
+import Img110 from "./profiles/110.jpg";
+import Img111 from "./profiles/111.jpg";
+import Img112 from "./profiles/112.jpg";
+import Img113 from "./profiles/113.jpg";
+import Img114 from "./profiles/114.jpg";
+import Img115 from "./profiles/115.jpg";
+import Img116 from "./profiles/116.jpg";
+import Img117 from "./profiles/117.jpg";
+import Img118 from "./profiles/118.jpg";
+import Img119 from "./profiles/119.jpg";
+import Img120 from "./profiles/120.jpg";
+import Img121 from "./profiles/121.jpg";
+import Img122 from "./profiles/122.jpg";
+import Img123 from "./profiles/123.jpg";
+import Img124 from "./profiles/124.jpg";
+import Img125 from "./profiles/125.jpg";
+import Img126 from "./profiles/126.jpg";
+import Img127 from "./profiles/127.jpg";
+import Img128 from "./profiles/128.jpg";
+import Img129 from "./profiles/129.jpg";
+import Img130 from "./profiles/130.jpg";
+import Img131 from "./profiles/131.jpg";
+import Img132 from "./profiles/132.jpg";
 
 export const AVATARS = [
     Img0,
@@ -102,4 +184,86 @@ export const AVATARS = [
     Img48,
     Img49,
     Img50,
+    Img51,
+    Img52,
+    Img53,
+    Img54,
+    Img55,
+    Img56,
+    Img57,
+    Img58,
+    Img59,
+    Img60,
+    Img61,
+    Img62,
+    Img63,
+    Img64,
+    Img65,
+    Img66,
+    Img67,
+    Img68,
+    Img69,
+    Img70,
+    Img71,
+    Img72,
+    Img73,
+    Img74,
+    Img75,
+    Img76,
+    Img77,
+    Img78,
+    Img79,
+    Img80,
+    Img81,
+    Img82,
+    Img83,
+    Img84,
+    Img85,
+    Img86,
+    Img87,
+    Img88,
+    Img89,
+    Img90,
+    Img91,
+    Img92,
+    Img93,
+    Img94,
+    Img95,
+    Img96,
+    Img97,
+    Img98,
+    Img99,
+    Img100,
+    Img101,
+    Img102,
+    Img103,
+    Img104,
+    Img105,
+    Img106,
+    Img107,
+    Img108,
+    Img109,
+    Img110,
+    Img111,
+    Img112,
+    Img113,
+    Img114,
+    Img115,
+    Img116,
+    Img117,
+    Img118,
+    Img119,
+    Img120,
+    Img121,
+    Img122,
+    Img123,
+    Img124,
+    Img125,
+    Img126,
+    Img127,
+    Img128,
+    Img129,
+    Img130,
+    Img131,
+    Img132,
 ];
